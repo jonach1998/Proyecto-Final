b56bc249e5d5eac516bc339296b1e8d601e0dc14
jonach1998 <40585571+jonach1998@users.noreply.github.com>
2018-12-10 21:27:01 -0600

    Add files via upload

---
+++ MICROS_RX/MICROS_RX.ino
@@ -0,0 +1,47 @@
+#include <EasyTransfer.h>
+
+//create object
+EasyTransfer ET; 
+
+struct RECEIVE_DATA_STRUCTURE{
+  //put your variable definitions here for the data you want to receive
+  //THIS MUST BE EXACTLY THE SAME ON THE OTHER ARDUINO
+  float lux;
+  float uv;
+  float temp0;
+  float temp1;
+  float temp2;
+  float tempprom;
+};
+
+//give a name to the group of data
+RECEIVE_DATA_STRUCTURE mydata;
+
+void setup(){
+  Serial.begin(9600);
+  //start the library, pass in the data details and the name of the serial port. Can be Serial, Serial1, Serial2, etc. 
+  ET.begin(details(mydata), &Serial);
+}
+
+void loop(){
+  //check and see if a data packet has come in. 
+  if(ET.receiveData()){
+    //this is how you access the variables. [name of the group].[variable name]
+    //since we have data, we will blink it out.
+    Serial.print("Lux: "); 
+    Serial.println(mydata.lux);
+    Serial.print("UV: ");
+    Serial.println(mydata.uv);
+    Serial.print("temp0: ");
+    Serial.println(mydata.temp0);
+    Serial.print("temp1: ");
+    Serial.println(mydata.temp1);
+    Serial.print("temp2: ");
+    Serial.println(mydata.temp2);
+    Serial.print("tempprom: ");
+    Serial.println(mydata.tempprom);
+  }
+  
+  //you should make this delay shorter then your transmit delay or else messages could be lost
+  delay(250);
+}
---
+++ MICROS_TX/MICROS_TX.ino
@@ -0,0 +1,114 @@
+#include <EasyTransfer.h>
+
+//create object
+EasyTransfer ET; 
+
+struct SEND_DATA_STRUCTURE{
+  //put your variable definitions here for the data you want to send
+  //THIS MUST BE EXACTLY THE SAME ON THE OTHER ARDUINO
+  float lux;
+  float uv;
+  float temp0;
+  float temp1;
+  float temp2;
+  float tempprom;
+};
+
+//give a name to the group of data
+SEND_DATA_STRUCTURE mydata;
+
+//Temp
+#include <OneWire.h> 
+#include <DallasTemperature.h>
+#include <Wire.h>
+//Lux
+#include <Adafruit_Sensor.h>
+#include <Adafruit_TSL2561_U.h>
+//UV
+#include "Adafruit_SI1145.h"
+//Temp
+#define stemp 2
+OneWire stemponewire(stemp);
+DallasTemperature sensors(&stemponewire);
+float promedio; 
+//Lux
+Adafruit_TSL2561_Unified tsl = Adafruit_TSL2561_Unified(TSL2561_ADDR_FLOAT, 12345);
+//UV
+Adafruit_SI1145 uv = Adafruit_SI1145();
+//BOMBA
+int bomba=5;
+
+void setup(){
+  Serial.begin(9600);
+  //start the library, pass in the data details and the name of the serial port. Can be Serial, Serial1, Serial2, etc.
+  ET.begin(details(mydata), &Serial);
+
+  pinMode(bomba,OUTPUT);
+  digitalWrite(bomba,LOW);
+  //Temp
+  sensors.begin(); 
+  //Lux
+    if(!tsl.begin())
+    {
+      Serial.print("Medidor de lux no esta funcionando");
+      while(1);
+    }
+    displaySensorDetails();
+    configureSensor();
+  //UV
+   if (! uv.begin()) {
+      Serial.println("Sensor UV no funciona");
+      while (1);
+    }
+}
+
+void loop(){
+//Temp
+sensors.requestTemperatures();
+  if(sensors.getTempCByIndex(0)!= -127 and sensors.getTempCByIndex(1) != -127 and sensors.getTempCByIndex(2) != -127)
+  {
+    digitalWrite(bomba,LOW);
+    promedio = (sensors.getTempCByIndex(0)+sensors.getTempCByIndex(1)+sensors.getTempCByIndex(2))/3;
+    //Lux
+    sensors_event_t event;
+    tsl.getEvent(&event);
+    //UV
+    float UVindex = uv.readUV(); 
+    UVindex = UVindex/100;
+    
+    //Lux
+      if (event.light)
+      {
+      mydata.lux=(event.light);
+      } 
+      else 
+      {
+      mydata.lux=0; 
+      }
+      mydata.uv=(UVindex);
+      mydata.temp0=sensors.getTempCByIndex(0); 
+      mydata.temp1=sensors.getTempCByIndex(1); 
+      mydata.temp2=sensors.getTempCByIndex(2); 
+      mydata.tempprom=(promedio); 
+      ET.sendData();
+      delay(100);
+  }
+  else 
+  Serial.println("Verifique sensores de temperatura");
+}
+
+//Lux
+void displaySensorDetails(void)
+{
+  sensor_t sensor;
+  tsl.getSensor(&sensor); 
+}
+
+void configureSensor(void)
+{
+  tsl.enableAutoRange(true);      
+  //tsl.setIntegrationTime(TSL2561_INTEGRATIONTIME_13MS);      /* fast but low resolution */
+  //tsl.setIntegrationTime(TSL2561_INTEGRATIONTIME_101MS);  /* medium resolution and speed   */
+  tsl.setIntegrationTime(TSL2561_INTEGRATIONTIME_402MS);  /* 16-bit data but slowest conversions */
+}
+//
